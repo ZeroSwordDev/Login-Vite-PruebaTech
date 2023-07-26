d4d1821d7890ed0a26d883b8166b9bd0336c7178
ZeroSwordDev <miguelworking1@gmail.com>
2023-07-26 13:26:04 -0400

    Login Prueba tecnica

---
+++ dafity/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <img
+            src="../src/assets/LOGO.png"
+            alt=""
+            className="image-ccu-absolute"
+          />
+          <div className="left">
+            <div className="text-container">
+              <h1 className="text-login">Login</h1>
+              <h3 className="text-gestion">Gestión</h3>
+              <p className="text-inventario">Inventario</p>
+            </div>
+            <div className="image-container-user">
+              <img src="../src/assets/user.png" alt="" className="image-user" />
+            </div>
+          </div>
+          <div className="right">
+            <div className="container-form">
+              <div className="User">
+                <div className="user-label">
+                  <label className="" htmlFor="user">
+                    Nombre de usuario
+                  </label>
+                </div>
+                <input
+                  className="input-user"
+                  id="user"
+                  type="text"
+                  placeholder="ej: Jhon"
+                />
+              </div>
+
+              <div className="password">
+                <div className="password-label">
+                  <label htmlFor="">Password</label>
+                </div>
+
+                <input
+                  className="input-password"
+                  type="text"
+                  placeholder="****"
+                />
+              </div>
+              <div className="password-record">
+                <a href="#">He olvidado mi contraseña</a>
+              </div>
+              <div className="container-buttom-form">
+                <button className="buttom-signIn">Iniciar session</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
